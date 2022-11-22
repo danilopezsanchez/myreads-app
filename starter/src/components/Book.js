372aeef862d72cf7097454e7afa11970f04c5976
Dani <danilopez.pc@gmail.com>
2022-11-22 16:09:34 +0100

    First commit with mock data

---
+++ starter/src/components/Book.js
@@ -0,0 +1,37 @@
+const Book = ({ book }) => {
+	const { title, author, img } = book;
+	const backgroundImg = `url(${img})`;
+	return(
+		<li>
+			<div className="book">
+				<div className="book-top">
+					<div
+					className="book-cover"
+					style={{
+						width: 128,
+						height: 193,
+						backgroundImage: backgroundImg,
+					}}
+					></div>
+					<div className="book-shelf-changer">
+					<select>
+						<option value="none" disabled>
+						Move to...
+						</option>
+						<option selected="selected" value="currentlyReading">
+						Currently Reading
+						</option>
+						<option value="wantToRead">Want to Read</option>
+						<option value="read">Read</option>
+						<option value="none">None</option>
+					</select>
+					</div>
+				</div>
+				<div className="book-title">{title}</div>
+				<div className="book-authors">{author}</div>
+			</div>
+		</li>
+	)
+}
+
+export default Book;
